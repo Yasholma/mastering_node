7508346bfffedd4e46d955f17f766be78b1ac4c4
Yahaya Hyellamoda Yohanna <hyohanna@bento.africa>
2021-04-16 23:59:55 +0100

    learn working with readable stream, enabling me to read a file without getting all the data at the same time

---
+++ 4-readable-streams.ts
@@ -0,0 +1,30 @@
+import * as fs from "fs";
+import { StringDecoder } from "string_decoder";
+import { Readable } from "stream";
+
+// const stream = fs.createReadStream("./file.txt", { encoding: "utf-8" });
+// const decoder = new StringDecoder("utf8");
+
+// stream.resume();
+
+// setTimeout(() => {
+//   stream.on("data", (chunk: Buffer) => {
+//     console.log(`Chunk: ${decoder.write(chunk)}`);
+//   });
+// }, 2000);
+
+const stream = new Readable();
+
+stream.push("hello");
+stream.push("world");
+stream.push(null);
+
+// stream.on("data", (chunk: Buffer) => {
+//   console.log(chunk.toString());
+// });
+stream.on("readable", () => {
+  let data;
+  while (null !== (data = stream.read())) {
+    console.log(data.toString());
+  }
+});
